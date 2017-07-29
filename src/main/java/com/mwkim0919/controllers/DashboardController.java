2f53525a169db8396a9560e9a1c544569d5c1589
Minwoo <mwkim0919@gmail.com>
2017-07-28 23:45:22 -0700

    User auth functionality done.

---
+++ src/main/java/com/mwkim0919/controllers/DashboardController.java
@@ -0,0 +1,29 @@
+package com.mwkim0919.controllers;
+
+import com.mwkim0919.models.User;
+import com.mwkim0919.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DashboardController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value="/dashboard", method = RequestMethod.GET)
+    public ModelAndView login(){
+        ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        modelAndView.addObject("userName", "Welcome " + user.getFirstName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
+        modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
+        modelAndView.setViewName("dashboard");
+        return modelAndView;
+    }
+}
